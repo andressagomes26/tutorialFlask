6077ae30d528d46513c99f4ccdf53fa6e9ff7520
Andressa Gomes <andressa_gomes.12@hotmail.com>
2022-02-03 20:06:39 -0300

    Itens B e C

--- a. What is Flask Python/server.py
+++ a. What is Flask Python/server.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Web App with Python Flask!'
+def hello_world(): return 'Hello World!'
 
-app.run(host='0.0.0.0', port=81)
+
+if __name__ == '__main__':
+    app.run()
