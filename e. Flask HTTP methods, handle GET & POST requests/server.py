eefba2f0897518727a67f972dcab94cc52f77aee
Andressa Gomes <andressa_gomes.12@hotmail.com>
2022-02-04 10:32:02 -0300

    "Item E - Flask HTTP methods"

--- e. Flask HTTP methods, handle GET & POST requests/server.py
+++ e. Flask HTTP methods, handle GET & POST requests/server.py
@@ -1,51 +1,22 @@
-# Flask Tutorial: Routes
-
-from flask import Flask
-from flask import render_template
-from flask import request
+# Flask HTTP methods, handle GET & POST requests
 
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-
-@app.route('/dashboard/<name>')
-def dashboard(name):
-    return 'welcome %s' % name
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        user = request.form['name']
-        return redirect(url_for('dashboard', name=user))
-    else:
-        user = request.args.get('name')
-        return render_template('login.html')
+   if request.method == 'POST':          # request.method == 'GET':
+      user = request.form['nm']
+      return redirect(url_for('success', name=user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success', name=user))
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
-
-
-
-'''
-@app.route('/hello')
-def hello_world():
-   return "hello world"
-
-
-@app.route('/product/<name>')
-def get_product(name):
-    return "O produto é " + str(name)
-
-
-@app.route('/sale/<transaction_id>')
-def get_sale(transaction_id=0):
-    return "The transaction is " + str(transaction_id)
-
-
-@app.route('/create/<first_name>/<last_name>')
-def create(first_name=None, last_name=None):
-    return 'Hello ' + first_name + ',' + last_name
-
-'''
+   app.run(debug=True)
